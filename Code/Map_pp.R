030b495a8ee4bcef1a283b71fa54744707e49fd3
Michiel van Dijk <michiel.vandijk@wur.nl>
2017-01-15 22:56:47 +0100

    Code for briefing paper

---
+++ Code/Map_pp.R
@@ -0,0 +1,28 @@
+#'========================================================================================================================================
+#' Project:  Test
+#' Subject:  Script to create maps
+#' Author:   Michiel van Dijk
+#' Contact:  michiel.vandijk@wur.nl
+#'========================================================================================================================================
+
+### PACKAGES
+if(!require(pacman)) install.packages("pacman")
+# Key packages
+p_load("tidyverse", "readxl", "stringr", "car", "scales", "RColorBrewer", "rprojroot")
+# Spatial packages
+p_load("rgdal", "ggmap", "raster", "rasterVis", "rgeos", "sp", "mapproj", "maptools", "proj4", "gdalUtils")
+# Additional packages
+#p_load("WDI", "countrycode")
+
+
+### DETERMINE ROOT PATH
+root <- find_root(is_rstudio_project)
+
+### DATAPATH
+dataPath <- "C:\\Users\\dijk158\\OneDrive - IIASA\\SurveyData"
+
+### R SETTINGS
+options(scipen=999) # surpress scientific notation
+options("stringsAsFactors"=FALSE) # ensures that characterdata that is loaded (e.g. csv) is not turned into factors
+options(digits=4)
+
